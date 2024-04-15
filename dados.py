9e185ee7dbd77daf4445293627adaeed447e990d
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-04-15 16:49:22 -0300

    Alterações

--- dados.py
+++ dados.py
@@ -7,5 +7,10 @@ df_total_acoes = pd.read_excel('D:\\1USUARIOMASTER12-01-2024\\Desktop\\Alura-Pyt
 #print(df_total_acoes) - Mostra a aba Total_de_acoes
 
 df_ticker = pd.read_excel('D:\\1USUARIOMASTER12-01-2024\\Desktop\\Alura-Python\\dados.xlsx.xlsx', sheet_name='Ticker')
-print(df_ticker)
+#print(df_ticker)
 
+df_principal = df_principal[['Ativo', 'Data', 'Último (R$)', 'Var. Dia (%)']].copy()
+#print(df_principal) - Seleciona colunas específicas
+
+df_principal = df_principal.rename(columns={'Último (R$)':'valor_final', 'Var. Dia (%)':'var_dia_pct'}).copy()
+#print(df_principal) - Renomeando as colunas
